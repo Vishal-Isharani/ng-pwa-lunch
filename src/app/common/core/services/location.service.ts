b3daecc8b5d95263885ef6e860219edd1a883a72
Cory Rylan <splintercode.cb@gmail.com>
2018-03-20 17:17:57 -0500

    feat: layout work

--- src/app/common/core/services/location.service.ts
+++ src/app/common/core/services/location.service.ts
@@ -7,16 +7,16 @@ export class LocationService {
   constructor() { }
 
   getLocation() {
-    // return new Observable<Coordinates>(observer => {
-    //   navigator.geolocation.getCurrentPosition(location => {
-    //     observer.next(location.coords);
-    //     observer.complete();
-    //   });
-    // });
-
-    return of({
-      latitude: 38.5285941,
-      longitude: -90.9974369
+    return new Observable<Coordinates>(observer => {
+      navigator.geolocation.getCurrentPosition(location => {
+        observer.next(location.coords);
+        observer.complete();
+      });
     });
+
+    // return of({
+    //   latitude: 38.5285941,
+    //   longitude: -90.9974369
+    // });
   }
 }
--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -1,4 +1,7 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-
-  constructor() { }
+  settingsForm: FormGroup;
+  distance: Observable<number>;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.settingsForm = this.formBuilder.group({
+      distance: [10]
+    });
+
+    this.distance = this.settingsForm.controls.distance.valueChanges.pipe(startWith(10));
   }
 
 }
